0c1b55696904d1f3c3d4a58c690375bf60bea11d
Andrea Dusza <andidusza@gmail.com>
2023-05-21 17:30:53 +0200

    Update user preferences

--- src/userconfig.andi.ts
+++ src/userconfig.andi.ts
@@ -6,7 +6,7 @@ export const AndiConfig = {
   warnList: [],
   blacklistExceptions: [],
   mehList: ['tarhonya'],
-  favList1: ['juhtúró', 'camembert', 'grill.{0,10}sajt', 'tápiókapuding', 'rák', 'garnéla', 'lazac', 'miso leves'],
-  favList2: ['aszalt paradicsom'],
+  favList1: ['juhtúró', 'gombafej', 'tápiókapuding', 'rák', 'garnéla', 'lazac', 'miso leves'],
+  favList2: ['aszalt paradicsom', 'camembert', 'négysajt', 'grill.{0,10}sajt', 'sajt.{0,5}töltött', 'sajtkrém', 'avokádó', 'kukoric(a|á)'],
   testingList: ['vega', 'vegán', 'vegetáriánus', 'akció', 'croissant'],
 } as const satisfies UserConfig;
